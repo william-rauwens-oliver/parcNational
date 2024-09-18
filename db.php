9f3b868d5a78671156a57f95f47cab1d9ddba76a
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-09-18 16:35:30 +0200

    delete arbo.

--- db.php
+++ db.php
@@ -8,14 +8,12 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Récupération des sentiers
     $sentiers = [];
     $sql = "SELECT nom_sentier, description, difficulte, longueur_km, points_interet, image, ville, pays FROM Sentier";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $sentiers = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Récupération des réservations de camping
     $reservation_camping = [];
     $sql = "SELECT date_reservation, date_debut, date_fin, statut, nombre_personnes, nom_camping, image FROM reservation_camping";
     $stmt = $pdo->prepare($sql);
