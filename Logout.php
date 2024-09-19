7d0fc563b3f30c8e3a0c8adf9ea4d654cd8187e6
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-09-19 10:32:43 +0200

    Add Redirection Login.

--- Logout.php
+++ Logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header("Location: index.php"); // Rediriger l'utilisateur vers la page d'accueil après déconnexion
+header("Location: Accueil.php");
 exit();
 ?>
